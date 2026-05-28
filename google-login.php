ac0e7d3ca8843711e6a2515d25aae487a30885fb
tttksaqa <tttksaqa@gmail.com>
2026-05-28 19:01:14 +1200

    Update google-login.php

--- google-login.php
+++ google-login.php
@@ -6,7 +6,7 @@ $client = new Google_Client();
 $client->setClientId("211181652280-mmom61lu3mfi2jn6hhjq0qft13n0tgih.apps.googleusercontent.com");
 $client->setClientSecret("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");
 
-$client->setRedirectUri("http://localhost/IS35126Group1/google-callback.php");
+$client->setRedirectUri("https://is35126group1-production.up.railway.app/google-callback.php");
 
 $client->addScope("email");
 $client->addScope("profile");
