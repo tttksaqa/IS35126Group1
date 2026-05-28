739de28263a05bf293796947de910a20148d8048
Teretianga Teannaki <tttksaqa@gmail.com>
2026-05-29 09:22:34 +1200

    Resolved db.php merge conflict

--- db.php
+++ db.php
@@ -1,27 +1,14 @@
 <?php
 $host = getenv("MYSQLHOST");
-<<<<<<< HEAD
+
 $user = getenv("MYSQLUSER") ?: "root";
 $pass = getenv("MYSQLPASSWORD") ?: getenv("MYSQL_ROOT_PASSWORD");
 $db   = getenv("MYSQLDATABASE");
 $port = getenv("MYSQLPORT");
 
 $conn = new mysqli($host, $user, $pass, $db, $port);
-=======
-$user = getenv("MYSQLUSER");
-$pass = getenv("MYSQLPASSWORD");
-$db   = getenv("MYSQLDATABASE");
-$port = getenv("MYSQLPORT");
->>>>>>> e1973107eba45d87d29e9506106c7dc702acdc23
 
-try {
-    $pdo = new PDO(
-        "mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4",
-        $user,
-        $pass
-    );
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Database connection failed.");
+if ($conn->connect_error) {
+    die("Database connection failed: " . $conn->connect_error);
 }
 ?>
