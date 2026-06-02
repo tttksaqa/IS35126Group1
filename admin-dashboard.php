d1a687bf32f9c485dc6799e04cc0f63cbbf219c0
tttksaqa <tttksaqa@gmail.com>
2026-06-02 16:32:10 +1200

    Update admin-dashboard.php

--- admin-dashboard.php
+++ admin-dashboard.php
@@ -1,23 +1,11 @@
 <?php
 session_start();
 
-if (!isset($_SESSION['user_id']) || 
-    !isset($_SESSION['role']) || 
-    $_SESSION['role'] !== 'admin' || 
-    !isset($_SESSION['otp_verified']) || 
-    $_SESSION['otp_verified'] !== true) {
-    
+if (!isset($_SESSION['role']) || $_SESSION['role'] != 'admin') {
     header("Location: login.php");
     exit();
 }
 ?>
-
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Admin Dashboard</title>
-</head>
-<body>
 
 <h1>Welcome Admin Dashboard</h1>
 
@@ -25,6 +13,3 @@ if (!isset($_SESSION['user_id']) ||
 <p>Your role is: <?php echo $_SESSION['role']; ?></p>
 
 <a href="logout.php">Logout</a>
-
-</body>
-</html>
