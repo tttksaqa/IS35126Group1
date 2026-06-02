f86ed41d159db09b109c76eef2eb2b51ffede95b
tttksaqa <tttksaqa@gmail.com>
2026-06-02 16:57:19 +1200

    Update db.php

--- db.php
+++ db.php
@@ -1,18 +1,10 @@
 <?php
 
-$url = getenv("DATABASE_URL");
-
-if (!$url) {
-    die("Database connection failed: DATABASE_URL is missing");
-}
-
-$parts = parse_url($url);
-
-$host = $parts["host"];
-$user = $parts["user"];
-$password = $parts["pass"];
-$database = ltrim($parts["path"], "/");
-$port = $parts["port"];
+$host = $_ENV['MYSQLHOST'] ?? getenv('MYSQLHOST');
+$user = $_ENV['MYSQLUSER'] ?? getenv('MYSQLUSER');
+$password = $_ENV['MYSQLPASSWORD'] ?? getenv('MYSQLPASSWORD');
+$database = $_ENV['MYSQLDATABASE'] ?? getenv('MYSQLDATABASE');
+$port = $_ENV['MYSQLPORT'] ?? getenv('MYSQLPORT');
 
 $conn = new mysqli($host, $user, $password, $database, $port);
 
@@ -20,4 +12,5 @@ if ($conn->connect_error) {
     die("Database connection failed: " . $conn->connect_error);
 }
 
+echo "Database Connected Successfully";
 ?>
