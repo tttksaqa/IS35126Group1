d6a677b5c959e04dc716ab225617e5f8ea3629a8
tttksaqa <tttksaqa@gmail.com>
2026-06-02 17:00:25 +1200

    Update db.php

--- db.php
+++ db.php
@@ -1,16 +1,14 @@
 <?php
 
-$host = $_ENV['MYSQLHOST'] ?? getenv('MYSQLHOST');
-$user = $_ENV['MYSQLUSER'] ?? getenv('MYSQLUSER');
-$password = $_ENV['MYSQLPASSWORD'] ?? getenv('MYSQLPASSWORD');
-$database = $_ENV['MYSQLDATABASE'] ?? getenv('MYSQLDATABASE');
-$port = $_ENV['MYSQLPORT'] ?? getenv('MYSQLPORT');
+$host = getenv("MYSQLHOST");
+$user = getenv("MYSQLUSER");
+$password = getenv("MYSQLPASSWORD");
+$database = getenv("MYSQLDATABASE");
+$port = getenv("MYSQLPORT");
 
 $conn = new mysqli($host, $user, $password, $database, $port);
 
 if ($conn->connect_error) {
     die("Database connection failed: " . $conn->connect_error);
 }
-
-echo "Database Connected Successfully";
 ?>
