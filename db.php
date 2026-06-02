7914eb72de8ff94da552bd11ca5f0ba4ea2daa47
tttksaqa <tttksaqa@gmail.com>
2026-06-02 18:22:22 +1200

    Update db.php

--- db.php
+++ db.php
@@ -1,29 +1,15 @@
 <?php
-
-$url = getenv("MYSQL_PUBLIC_URL");
-
-if (!$url) {
-    die("Database connection failed: MYSQL_PUBLIC_URL is missing");
-}
-
-$parts = parse_url($url);
-
-$host = $parts["host"];
-$user = $parts["user"];
-$password = $parts["pass"];
-$database = ltrim($parts["path"], "/");
-$port = $parts["port"];
+$host = getenv('MYSQLHOST');
+$dbname = getenv('MYSQLDATABASE');
+$user = getenv('MYSQLUSER');
+$password = getenv('MYSQLPASSWORD');
+$port = getenv('MYSQLPORT');
 
 try {
-    $conn = new PDO(
-        "mysql:host=$host;port=$port;dbname=$database;charset=utf8mb4",
-        $user,
-        $password
-    );
-
+    $conn = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4", $user, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
 } catch (PDOException $e) {
     die("Database connection failed: " . $e->getMessage());
 }
 ?>
+ 
