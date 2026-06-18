8c77b9167f481e8904896c549b36785059b8f4f9
tttksaqa <tttksaqa@gmail.com>
2026-06-18 12:22:00 +1200

    Update db.php

--- db.php
+++ db.php
@@ -9,7 +9,6 @@ try {
     $conn = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4", $user, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    die("Database connection failed: " . $e->getMessage());
+    die("Database connection failed.");
 }
-?>
  
